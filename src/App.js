7fb0789e0e5e38b69c407448b0e30b8819c9a9da
pastelceo <pastelceo@gmail.com>
2025-08-28 20:37:07 +0900

    Initial commit: Add coupon settlement system

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CouponSettlementSystem from './CouponSettlementSystem';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CouponSettlementSystem />
+    </div>
+  );
+}
+
+export default App;
